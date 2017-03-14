a4a0786f6efd3a679a00a78ddc2c377012e957d9
djudjuu <djuju@thinki.org>
2017-03-14 18:07:25 +0100

    better questions

--- libsolidity/ast/ASTJsonConverter.cpp
+++ libsolidity/ast/ASTJsonConverter.cpp
@@ -131,15 +131,20 @@ bool ASTJsonConverter::visit(PragmaDirective const& _node)
 
 bool ASTJsonConverter::visit(ImportDirective const& _node)
 {
+  	/*
 	Json::Value symbolAliases(Json::objectValue);
 	for (auto const& symbolAlias: _node.symbolAliases())
-        	symbolAliases.append()        //??: how to cast symbolAlias[0] and symbolAlias[1] to a pair that i can append to json::objectValue?
-        //another concern I am having is that these could be partiall or completely nil
-            
+        	//??: how to cast symbolAlias[0] and symbolAlias[1] to a pair that i can append to json::objectValue?
+        	symbolAliases.append( ??? )        
+
+        //another concern I am having is that these could be partially or completely nil
+        */    
 	addJsonNode(_node, "ImportDirective", {
         	make_pair("file", _node.path()),
-                  make_pair("absolutePath", _node.annotation().absolutePath)
-                  //make_pair("SourceUnit", _node.annotation().sourceUnit),  // this just translates to 'true', how
+                  make_pair("absolutePath", _node.annotation().absolutePath),
+                  make_pair("SourceUnit", _node.annotation().sourceUnit->id())
+                  // is that enough to identify it by?
+                  //maybe
                   //make_pair("symbolAliases", symbolAliases) //see above
 	});
 	return true;
